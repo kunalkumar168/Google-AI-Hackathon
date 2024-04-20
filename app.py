de4e6807ab41b471722ded090c64e2f453c8c5ee
kunalkumar168 <kunal.kumar.eee14@itbhu.ac.in>
2024-04-20 22:23:26 +0000

    updating app.py

--- app.py
+++ app.py
@@ -121,5 +121,4 @@ def generate():
 
 
 if __name__ == '__main__':
-    #app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
-    app.run(debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
